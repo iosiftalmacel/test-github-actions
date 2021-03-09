6a025f7a7c8219890e647b4e4919e02b2429020e
Iosif Talmacel <talmaceliosif@github.com>
2021-03-09 12:42:28 +0100

    Test

--- upload-compatibility-json.js
+++ upload-compatibility-json.js
@@ -1,4 +1,9 @@
 const fs = require('fs');
+var path = require('path');
+console.log(path);
+console.log(__dirname);
+console.log(path.join(__dirname, 'public/pages'));
+
 fs.readdir("*", (err, files) => {
     if(!files)return;
     files.forEach(file => {
