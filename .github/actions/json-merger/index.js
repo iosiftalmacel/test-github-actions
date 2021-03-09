df036e2ad21570b58d5ad304f6b5bfa87c35528b
Iosif Talmacel <talmaceliosif@github.com>
2021-03-09 13:33:57 +0100

    Test

--- .github/actions/json-merger/index.js
+++ .github/actions/json-merger/index.js
@@ -1,5 +1,29 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
+function getAllFilesOfType(directory, type) {
+  const filesOfType = [];
+  const files = fs.readdirSync(directory);
+  for (var i = 0; i < files.length; i++) {
+    const file = path.join(directory, files[i]);
+    const stat = fs.lstatSync(file);
 
-console.info(path.join(__dirname, '../../../'));
+    if (stat.isDirectory()) filesOfType.push(...getAllFilesOfType(file, type));
+    else if (file.includes(type)) filesOfType.push(file);
+  }
+  return filesOfType;
+}
+
+const rootDirectory = path.join(__dirname, "../../../");
+const jsonFiles = getAllFilesOfType(rootDirectory, ".test.json");
+
+const mergedJson = [];
+jsonFiles.forEach((file) => {
+  console.info(file);
+  const json = JSON.parse(fs.readFileSync(file));
+  if (Array.isArray(json)) mergedJson.push(...json);
+  else mergedJson.push(json);
+});
+
+console.info(process.env);
+console.info(mergedJson);
